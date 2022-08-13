101b975687316c8dd80be742d0f2a155c68302fe
Daniel Ricq <dricq@findusapp.de>
2022-08-13 10:00:46 +0200

    feat: Add an internal todo list and routes...
    ... to access those todos

--- api/src/index.js
+++ api/src/index.js
@@ -7,9 +7,36 @@ const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const todos = {
+  1: {
+    id: "1",
+    description: "First Todo",
+  },
+  2: {
+    id: "2",
+    description: "Second Todo",
+  },
+};
+
+app.get("/todos", (req, res) => {
+  return res.send(Object.values(todos));
 });
+
+app.get("/todos/:todoId", (req, res) => {
+  return res.send(todos[req.params.todoId]);
+});
+
+// app.post("/todos", (req, res) => {
+//   return res.send("POST HTTP method on user resource");
+// });
+
+// app.put("/todos/:todoId", (req, res) => {
+//   return res.send(`PUT HTTP method on todos/${req.params.todoId} resource`);
+// });
+
+// app.delete("/todos/:todoId", (req, res) => {
+//   return res.send(`DELETE HTTP method on todos/${req.params.todoId} resource`);
+// });
 
 app.listen(process.env.PORT, () =>
   console.log(
