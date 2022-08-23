e28c076f9e0a54b027b0b888b9f75e936f06038f
Daniel Ricq <dricq@findusapp.de>
2022-08-23 15:45:25 +0200

    feat: Add markup for the todo list

---
+++ client/src/components/TodoList/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <section aria-labelledby="heading">
+      <h1 id="heading">Todo List</h1>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+    </section>
+  );
+};
+
+export { TodoList };
